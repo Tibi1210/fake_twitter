d84adbe68cba8da779eee4e106b626c7b95e5d95
Vajda Tibor <h053353@stud.u-szeged.hu>
2022-03-29 14:04:17 +0200

    top navbar changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { TopmenuComponent } from './shared/topmenu/topmenu.component';
 import { TweetcardComponent } from './shared/tweetcard/tweetcard.component';
 import { LeftmenuComponent } from './shared/leftmenu/leftmenu.component';
+import { ToptitleComponent } from './shared/toptitle/toptitle.component';
+import { TopsearchComponent } from './shared/topsearch/topsearch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopmenuComponent,
     TweetcardComponent,
-    LeftmenuComponent
+    LeftmenuComponent,
+    ToptitleComponent,
+    TopsearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/toptitle/toptitle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toptitle',
+  templateUrl: './toptitle.component.html',
+  styleUrls: ['./toptitle.component.scss']
+})
+export class ToptitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
