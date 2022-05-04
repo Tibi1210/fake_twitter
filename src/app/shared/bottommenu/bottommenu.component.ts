7444f31fb61ebdc47854d7f61022b6038fa9495e
Vajda Tibor <h053353@stud.u-szeged.hu>
2022-05-04 21:15:56 +0200

    mobile first

--- src/app/shared/bottommenu/bottommenu.component.ts
+++ src/app/shared/bottommenu/bottommenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-bottommenu',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BottommenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    this.authService.isUserLoggedIn().subscribe(
+      (user) => {
+        if(user){
+          this.authService.logout();
+          localStorage.setItem('user', JSON.parse('null'));
+          localStorage.setItem('userData', JSON.parse('null'));
+        }
+      },
+      (error) => {
+        console.error(error);
+        localStorage.setItem('user', JSON.parse('null'));
+        localStorage.setItem('userData', JSON.parse('null'));
+      }
+    );    
   }
 
 }
